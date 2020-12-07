30eafa9adeeb5fef201a5bd50b69242c931e6ef6
Katarzyna <lela900321@gmail.com>
2020-12-07 14:43:58 +0100

    Add pictures; fix front page styles

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'onlineTShirtShop'`, () => {
+  it(`should have as title 'sneakersShoesShop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('onlineTShirtShop');
+    expect(app.title).toEqual('sneakersShoesShop');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to onlineTShirtShop!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sneakersShoesShop!');
   });
 });
